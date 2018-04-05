d1212edb2cd459be2aff079b00edbeeadb6fc91e
edward3h <jaq+github@ethelred.org>
2018-04-05 12:28:15 -0400

    initial version of rules display. lame

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TitleList from './titlelist';
+import RulesList from './ruleslist';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends React.Component {
-	render() {
-		return (
-			<div>
-			<TitleList/>
+const App = () => (
+	<Router>
+		<div>
+			<div className="links"><Link to="/">Raw Feed</Link> | <Link to="/rules">Rules</Link></div>
+			<div className="body">
+				<Route exact path="/" component={TitleList} />
+				<Route path="/rules" component={RulesList} />
 			</div>
-		);
-	}
-}
+		</div>
+	</Router>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ client/src/ruleslist.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import update from 'immutability-helper';
+
+const PAGE_SIZE = 20;
+const TIMEOUT = 3000;
+
+function ListItem(props) {
+	return (
+		<tr>
+		<td>{props.name}</td>
+		<td>{props.pattern}</td>
+		<td>{props.action}</td>
+		</tr>
+	);
+}
+
+export default class RulesList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: [],
+			offset: 0
+		};
+	}
+
+	componentDidMount() {
+		this.doRulesFetch();
+	}
+
+	doRulesFetch(offset = this.state.offset) {
+		const qs = offset > 0 ? `?offset=${offset}` : "";
+		fetch(`/api/rules${qs}`)
+	        .then(results => {
+	                return results.json();
+	        }).then(data => {
+	                this.setState({items: data, offset: offset});
+	        });
+	}
+
+	render() {
+		let listitems = this.state.items.map((item) =>
+			<ListItem key={item.id}	{...item} />
+		);
+		if (this.state.items.length == 0) {
+			listitems = <div>Please wait, loading <img width="20" height="20" src="spinner.gif"/></div>;
+		}
+
+		const paging = (
+			<div className="pagination">
+			{this.state.offset > 0 &&
+				<button className="previous" onClick={() => this.doRulesFetch(this.state.offset - PAGE_SIZE)}>⬅️ Previous</button>
+			}
+			{this.state.items.length == PAGE_SIZE && 
+				<button className="next" onClick={() => this.doRulesFetch(this.state.offset + PAGE_SIZE)}>Next ➡️</button>
+			}
+			</div>
+		);
+		return (
+		<div className="listouter">
+			<table>
+			<thead>
+			<tr><th>Name</th><th>Pattern</th><th>Action</th></tr>
+			</thead>
+			<tbody>
+			{listitems}
+			</tbody>
+			</table>
+			{paging}
+		</div>
+		);
+	}
+}
