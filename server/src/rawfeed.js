61776647fc6ae65842f59e5426836fc90ce9a49a
edward3h <jaq+github@ethelred.org>
2018-10-13 22:44:19 -0400

    implement search on title

--- server/src/rawfeed.js
+++ server/src/rawfeed.js
@@ -3,9 +3,17 @@ const router = express.Router();
 import db from '../../src/db'; // TODO make this nicer?
 import downloads from '../../src/downloads';
 
+const BASE_QUERY = 'select id, title, description, transmission_id from rawfeed left outer join downloads on rawfeed.id = downloads.feed_id where not hidden';
+const SEARCH_QUERY = `${BASE_QUERY} and title like ?`;
+
+const transformSearch = (input) => `%${input}%`.replace(/\W+/gi, '%');
+
 router.get('/',  (req, res) => {
 	const offset = parseInt(req.query.offset, 10) || 0;
-	db.query("select id, title, description, transmission_id from rawfeed left outer join downloads on rawfeed.id = downloads.feed_id where not hidden order by created_at desc, id desc limit 20 offset ?", [offset], 
+	const search = req.query.search;
+	const args = search ? [transformSearch(search), offset] : [offset];
+
+	db.query(`${search ? SEARCH_QUERY : BASE_QUERY} order by created_at desc, id desc limit 20 offset ?`, args, 
 		(err, results, fields) => { 
 		if(err) {
 			console.log(err);
